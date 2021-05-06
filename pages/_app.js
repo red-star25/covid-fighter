2f65c844d48137156ff3961b698d30243b13409e
red-star25 <nakumdhruv123@gmail.com>
2021-05-06 23:15:28 +0530

    Ui fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
+import Header from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Covid Fighter</title>
+      </Head>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;
